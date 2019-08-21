32efa5397e79ef220123030f72079515160096f3
Karolina Poslek <karolina@poslek.com>
2019-08-21 23:58:04 +0200

    publish robofriends

---
+++ precache-manifest.6e0675d93ebd3a7fa724d1150a0c8136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5541d48c3a8f8b150b3deaa5a363a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13464a649aa1c96fd8b0",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "431fcaf308d4cd1e69d4",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "13464a649aa1c96fd8b0",
+    "url": "/static/js/2.541164eb.chunk.js"
+  },
+  {
+    "revision": "431fcaf308d4cd1e69d4",
+    "url": "/static/js/main.74274d6e.chunk.js"
+  },
+  {
+    "revision": "b0a546bcc125fb21d6be",
+    "url": "/static/js/runtime~main.75271b63.js"
+  }
+]);

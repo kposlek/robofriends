398efb02395ac44af01a6c1a2ff0237a9abcfdad
Karolina Poslek <karolina@poslek.com>
2019-08-21 13:20:30 +0200

    settings 'Card' component

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Card = () => {
+    return (
+        <div>
+            <img src="" />
+            <div>
+                <h2>Kristian Poslek</h2>
+                <p>kristian@poslek.com</p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;

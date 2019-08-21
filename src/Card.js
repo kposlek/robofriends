5b2e69c1c2ed3fbe9a45588a83caf3ad8828112b
Karolina Poslek <karolina@poslek.com>
2019-08-21 15:58:02 +0200

    linking 'robots' and 'Card' component

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,12 @@
 import React from 'react';
-const Card = () => {
+import { tsPropertySignature } from '@babel/types';
+const Card = props => {
     return (
-        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <img alt="robots" src="https://robohash.org/test?size=200x200" />
             <div>
-                <h2>Kristian Poslek</h2>
-                <p>kristian@poslek.com</p>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
         </div>
     );
